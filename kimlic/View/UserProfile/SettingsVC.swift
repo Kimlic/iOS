21a11ce4dd6c00f91f363533a8b31bb43d409fa6
izzet öztürk <izzeto@ratel.com.tr>
2018-06-11 19:49:10 +0300

    #24 Screen mocup desiged

--- kimlic/View/UserProfile/SettingsVC.swift
+++ kimlic/View/UserProfile/SettingsVC.swift
@@ -18,3 +18,50 @@ class SettingsVC: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
 }
+
+class SettingsTableVC: UITableViewController {
+    
+    @IBOutlet weak var warningIconPasscode: UIImageView!
+    @IBOutlet weak var warningIconAccount: UIImageView!
+    @IBOutlet weak var warningIconTouchID: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.backgroundColor = UIColor.clear
+        cell.selectionStyle = .none
+        
+        if let warningIcon = cell.viewWithTag(100) as? UIImageView, let switchButton = cell.viewWithTag(101) as? UISwitch {
+            warningIcon.isHidden = switchButton.isOn
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 3:
+            print("Terms & Conditions Clicked")
+        case 4:
+            print("About Kimlic Clicked")
+        default:
+            break
+        }
+    }
+    
+    @IBAction func touchIDSwitchChange(_ sender: UISwitch) {
+        warningIconTouchID.isHidden = sender.isOn
+    }
+    
+    @IBAction func accountSwitchChange(_ sender: UISwitch) {
+        warningIconAccount.isHidden = sender.isOn
+    }
+    
+    @IBAction func passcodeSwitchChange(_ sender: UISwitch) {
+        warningIconPasscode.isHidden = sender.isOn
+    }
+    
+    
+}
